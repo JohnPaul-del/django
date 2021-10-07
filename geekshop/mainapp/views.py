68a673119e6e4aa2235c4a224f9ed669ccbb8225
Ivan <ivan.pavlov.alk@gmail.com>
2021-10-07 07:26:41 +0200

    L2

--- geekshop/mainapp/views.py
+++ geekshop/mainapp/views.py
@@ -2,4 +2,16 @@ from django.shortcuts import render
 
 
 def products(request):
-    return render(request, "mainapp/products.html")
+    title = 'Catalog'
+    links_menu = [
+        {'href': 'products', 'name': 'all'},
+        {'href': 'products_home', 'name': 'home'},
+        {'href': 'products_office', 'name': 'office'},
+        {'href': 'products_classic', 'name': 'classic'},
+    ]
+
+    context = {
+        'title': title,
+        'links_menu': links_menu,
+    }
+    return render(request, "mainapp/products.html", context)
