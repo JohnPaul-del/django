78377c5fa1764b62d859d5f3973af4eb7b914317
Ivan <ivan.pavlov.alk@gmail.com>
2021-10-09 19:06:22 +0200

    Lesson 3 - Completed

--- geekshop/mainapp/models.py
+++ geekshop/mainapp/models.py
@@ -1,3 +1,42 @@
 from django.db import models
 
-# Create your models here.
+
+class ProductCategory(models.Model):
+    name = models.CharField(
+        max_length=64,
+        verbose_name='name',
+        unique=True)
+    description = models.TextField(
+        verbose_name='description',
+        blank=True)
+
+    def __str__(self):
+        return self.name
+
+
+class Product(models.Model):
+    category = models.ForeignKey(ProductCategory, on_delete=models.CASCADE)
+    name = models.CharField(
+        max_length=128,
+        verbose_name='product_name')
+    image = models.ImageField(
+        upload_to='products_images',
+        blank=True)
+    short_desc = models.CharField(
+        max_length=60,
+        verbose_name='product preview description',
+        blank=True)
+    description = models.TextField(
+        verbose_name='product description',
+        blank=True)
+    price = models.DecimalField(
+        max_digits=8,
+        verbose_name='product price',
+        decimal_places=2,
+        default=0)
+    quantity = models.PositiveIntegerField(
+        verbose_name='product quantity',
+        default=0)
+
+    def __str__(self):
+        return f"{self.name} ({self.category.name})"
--- geekshop/mainapp/urls.py
+++ geekshop/mainapp/urls.py
@@ -1,6 +1,9 @@
 from django.urls import path
-from .views import products
+import mainapp.views as mainapp
+
+app_name = 'mainapp'
 
 urlpatterns = [
-     path('', products, name='products'),
+     path('', mainapp.products, name='products'),
+     path('<int:pk>/', mainapp.products, name='category')
 ]
